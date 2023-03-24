c48fdfd1473cd69be0b7ecaa44665dcfbcbc0922
farmanaug91 <128634779+farmanaug91@users.noreply.github.com>
2023-03-24 16:00:54 -0400

    Testing Continuous Delivery in GCP Cloud Build

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Continuous Delivery Check'
+    return 'Hello World! Continuous Delivery Check1'
 
 @app.route('/echo/<name>')
 def echo(name):
